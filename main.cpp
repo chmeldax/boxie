20a8bb9b0200693e2aa4cbbe1da4c91984566553
Jan Chmelicek <jan.chmelicek@simplybusiness.co.uk>
2016-03-21 12:32:21 +0000

    Introducing uts namespace

--- main.cpp
+++ main.cpp
@@ -1,26 +1,40 @@
-#include<iostream>
-#include<sched.h>
-#include<stdlib.h>
-#include<sys/wait.h>
-#include<errno.h>
-#include<unistd.h>
+#include <iostream>
+#include <sched.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+#include <string>
 
 int run(void* arg)
 {
-	sleep(1);
+	const std::string name = "funny-name";
+	struct utsname uts;
+	sethostname((const char *) name.c_str(), name.size());
+	
 	std::cout << "This is child" << std::endl;
+	uname(&uts);
+	std::cout << "This is a child's nodename: " << uts.nodename << std::endl;
+	
 	return 0;
 }
 
 int main()
 {
+	struct utsname uts;
+	
 	char *stack = (char*) malloc(1024*1024);
 	std::cout << "This is parent" << std::endl;
-	pid_t pid = clone(run, (stack + 1024*1024), CLONE_NEWNS | SIGCHLD, NULL);
+	pid_t pid = clone(run, (stack + 1024*1024), CLONE_NEWUTS | SIGCHLD, NULL);
 	if (pid == -1)
 	{
 		std::cout << "Creating new namespace failed. Error number: " << errno;
 	}
+	
 	sleep(1);
+	
+	uname(&uts);
+	std::cout << "This is a parent's nodename: " << uts.nodename << std::endl;
 	waitpid(pid, NULL, 0);
 }
