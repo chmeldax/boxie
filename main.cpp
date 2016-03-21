a1f66c9d12955e3b994ae2e5903155a75b27cd49
Jan Chmelicek <jan.chmelicek@simplybusiness.co.uk>
2016-03-21 13:19:56 +0000

    Convert indentation

--- main.cpp
+++ main.cpp
@@ -10,51 +10,51 @@
 int run(void* arg)
 {
     char **argv = (char**) arg;
-	const std::string name = "funny-name";
-	struct utsname uts;
-	sethostname((const char *) name.c_str(), name.size());
-	
-	std::cout << "This is child" << std::endl;
-	uname(&uts);
-	std::cout << "This is a child's nodename: " << uts.nodename << std::endl;
-	
-	pid_t pid = fork();
+    const std::string name = "funny-name";
+    struct utsname uts;
+    sethostname((const char *) name.c_str(), name.size());
+    
+    std::cout << "This is child" << std::endl;
+    uname(&uts);
+    std::cout << "This is a child's nodename: " << uts.nodename << std::endl;
+    
+    pid_t pid = fork();
     if (pid == 0)
     {
-		std::cout << "About to spin a new task" << std::endl;
-		std::cout << argv[1] << std::endl;
-		execvp(argv[1], &argv[1]);
+        std::cout << "About to spin a new task" << std::endl;
+        std::cout << argv[1] << std::endl;
+        execvp(argv[1], &argv[1]);
     }
     else if (pid > 0)
     {
         // parent process
-		waitpid(pid, NULL, 0);
+        waitpid(pid, NULL, 0);
     }
     else
     {
-		std::cout << "Forking failed" << std::endl;
+        std::cout << "Forking failed" << std::endl;
         return 1;
     }
-	
-	return 0;
+    
+    return 0;
 }
 
 
 int main(int argc, char *argv[])
 {
-	struct utsname uts;
-	
-	char *stack = (char*) malloc(1024*1024);
-	std::cout << "This is parent" << std::endl;
-	pid_t pid = clone(run, (stack + 1024*1024), CLONE_NEWUTS | SIGCHLD, argv);
-	if (pid == -1)
-	{
-		std::cout << "Creating new namespace failed. Error number: " << errno;
-	}
-	
-	sleep(1);
-	
-	uname(&uts);
-	std::cout << "This is a parent's nodename: " << uts.nodename << std::endl;
-	waitpid(pid, NULL, 0);
+    struct utsname uts;
+    
+    char *stack = (char*) malloc(1024*1024);
+    std::cout << "This is parent" << std::endl;
+    pid_t pid = clone(run, (stack + 1024*1024), CLONE_NEWUTS | SIGCHLD, argv);
+    if (pid == -1)
+    {
+        std::cout << "Creating new namespace failed. Error number: " << errno;
+    }
+    
+    sleep(1);
+    
+    uname(&uts);
+    std::cout << "This is a parent's nodename: " << uts.nodename << std::endl;
+    waitpid(pid, NULL, 0);
 }
