ccd643c9b28d23ecd5a55471890cda81dcb6e804
Jan Chmelicek <jan.chmelicek@simplybusiness.co.uk>
2016-03-21 15:24:20 +0000

    Container object introduced, still a bit mess

--- main.cpp
+++ main.cpp
@@ -6,24 +6,54 @@
 #include <unistd.h>
 #include <sys/utsname.h>
 #include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
 
-int run(void* arg)
+class Container
 {
-    char **argv = (char**) arg;
-    const std::string name = "funny-name";
-    struct utsname uts;
-    sethostname((const char *) name.c_str(), name.size());
+public:
+    class Builder;
+    void run_command(std::string command, std::vector<std::string> args);
     
-    std::cout << "This is child" << std::endl;
-    uname(&uts);
-    std::cout << "This is a child's nodename: " << uts.nodename << std::endl;
+private:
+    Container(std::string hostname) : hostname(hostname) {};
+    std::string hostname;
+    void change_hostname(std::string hostname);
+};
+
+class Container::Builder
+{
+public:
+    Builder& set_hostname(const std::string& hostname) { this->hostname = hostname; return *this; } 
+    Container build() { return Container(hostname); }
     
+private:
+    std::string hostname;
+};
+
+void Container::run_command(std::string command, std::vector<std::string> args)
+{
+    std::vector<char*> args_char;
+    try {
+        std::transform(args.begin(), args.end(), std::back_inserter(args_char), [](const std::string & s) -> char* { 
+            char *pc = new char[s.size() + 1];
+            std::strcpy(pc, s.c_str());
+            return pc;
+            }
+        );
+    }
+    catch (std::exception const &exc)
+    {
+        std::cerr << "Exception caught " << exc.what() << "\n";
+    }
+
     pid_t pid = fork();
     if (pid == 0)
     {
         std::cout << "About to spin a new task" << std::endl;
-        std::cout << argv[1] << std::endl;
-        execvp(argv[1], &argv[1]);
+        std::cout << command << std::endl;
+        execvp((const char*) command.c_str(), &args_char[0]);
     }
     else if (pid > 0)
     {
@@ -33,10 +63,38 @@ int run(void* arg)
     else
     {
         std::cout << "Forking failed" << std::endl;
-        return 1;
+    }
+}
+
+void Container::change_hostname(std::string hostname)
+{
+    sethostname((const char *) hostname.c_str(), hostname.size());
+}
+
+
+int run(void* arg)
+{
+    const std::string name = "funny-name";
+    char **argv = (char**) arg;
+    
+    Container::Builder builder;
+    builder.set_hostname(name);
+    Container container = builder.build();
+    
+    std::vector<std::string> args(1);
+    try 
+    {
+        container.run_command((std::string) argv[1], args);
+    }
+    catch (std::exception const &exc)
+    {
+        std::cerr << "Exception caught " << exc.what() << "\n";
     }
     
-    return 0;
+    struct utsname uts;
+    std::cout << "This is child" << std::endl;
+    uname(&uts);
+    std::cout << "This is a child's nodename: " << uts.nodename << std::endl;
 }
 
 
