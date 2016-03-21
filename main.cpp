9eba2aa16b5004747993772ca5ecef4dd7e63ccd
Jan Chmelicek <jan.chmelicek@simplybusiness.co.uk>
2016-03-21 13:08:34 +0000

    Exec command in namespace

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 
 int run(void* arg)
 {
+    char **argv = (char**) arg;
 	const std::string name = "funny-name";
 	struct utsname uts;
 	sethostname((const char *) name.c_str(), name.size());
@@ -17,16 +18,35 @@ int run(void* arg)
 	uname(&uts);
 	std::cout << "This is a child's nodename: " << uts.nodename << std::endl;
 	
+	pid_t pid = fork();
+    if (pid == 0)
+    {
+		std::cout << "About to spin a new task" << std::endl;
+		std::cout << argv[1] << std::endl;
+		execvp(argv[1], &argv[1]);
+    }
+    else if (pid > 0)
+    {
+        // parent process
+		waitpid(pid, NULL, 0);
+    }
+    else
+    {
+		std::cout << "Forking failed" << std::endl;
+        return 1;
+    }
+	
 	return 0;
 }
 
-int main()
+
+int main(int argc, char *argv[])
 {
 	struct utsname uts;
 	
 	char *stack = (char*) malloc(1024*1024);
 	std::cout << "This is parent" << std::endl;
-	pid_t pid = clone(run, (stack + 1024*1024), CLONE_NEWUTS | SIGCHLD, NULL);
+	pid_t pid = clone(run, (stack + 1024*1024), CLONE_NEWUTS | SIGCHLD, argv);
 	if (pid == -1)
 	{
 		std::cout << "Creating new namespace failed. Error number: " << errno;
